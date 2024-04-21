07b406eb160c9bd4c67f45a2b90c5e7b3a63dd3f
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2024-04-21 07:46:03 -0400

    update

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,29 +1,21 @@
 import React from "react";
+import StepLoader from "../component/StepLoader";
+import Loading from "../component/Loading";
 
-const API_URL = "https://2677-131-123-52-17.ngrok-free.app ";
+const API_URL = "https://af57-131-123-52-40.ngrok-free.app";
+
+interface Response {
+  contents: string[];
+}
 
 export default function List() {
-  const [step, setStep] = React.useState(0);
-  const [fileUploaded, setFileUploaded] = React.useState();
-  const hiddenFileInput = React.useRef(null);
+  const [step, setStep] = React.useState<number>(0);
 
   const handleNextStep = () => {
-    setStep((prevStep) => prevStep + 1);
+    setStep(step + 1);
   };
 
-  const handleClick = (event: any) => {
-    if (hiddenFileInput.current) {
-      // @ts-ignore
-      hiddenFileInput.current.click();
-    }
-  };
-
-  const handleChange = (event: any) => {
-    console.log("File selected:", event.target.files[0]);
-    setFileUploaded(event.target.files[0]);
-  };
-
-  const [response, setResponse] = React.useState();
+  const [response, setResponse] = React.useState<Response>();
   const [loading, setLoading] = React.useState(false);
 
   const handleFormSubmit = async (event: any) => {
@@ -45,12 +37,34 @@ export default function List() {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const responseData = await response.json(); // Parse response data as JSON
-      setResponse(responseData); // Update state with response data
       console.log("Response data:", responseData);
+      handleNextStep();
+      handleRecipeGenerate(responseData); // Call handleRecipeGenerate function with response data as argument
       setLoading(false); // Set loading state to false
     } catch (error) {
       console.error("Error fetching data:", error);
       setLoading(false); // Set loading state to false
+    }
+  };
+
+  const handleRecipeGenerate = async (res: any) => {
+    try {
+      const response = await fetch(`${API_URL}/ingredent2recipe`, {
+        method: "POST",
+        body: JSON.stringify(res),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const responseData = await response.json(); // Parse response data as JSON
+      console.log("Response data:", responseData);
+      setResponse(responseData); // Set response state with response data
+      handleNextStep();
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
   };
 
@@ -59,48 +73,57 @@ export default function List() {
   }
 
   return (
-    <div className="flex flex-col min-h-[100dvh] items-center justify-center">
-      {/* take photo of fridge */}
-      {step === 0 && (
-        <div className="flex flex-col items-center justify-center space-y-4">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-            Take a photo of your fridge
-          </h1>
-          <form
-            onSubmit={handleFormSubmit}
-            className="flex flex-col justify-center items-center space-y-4"
-          >
-            <button
-              onClick={handleClick}
-              className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 py-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
+    <div>
+      <StepLoader state={step} />
+      <div className="flex flex-col min-h-[100dvh-1.5rem] items-center justify-center">
+        {/* take photo of fridge */}
+        {step === 0 && (
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              Take a photo of your fridge
+            </h1>
+            <form
+              onSubmit={handleFormSubmit}
+              className="flex flex-col justify-center items-center space-y-4"
             >
-              Open Camera
-            </button>
-            <input
-              name="image"
-              accept="image/*"
-              id="icon-button-file"
-              type="file"
-              capture="environment"
-            />
-            <button
-              type="submit"
-              className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 py-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-      )}
-      {/* list ingredients */}
-      {step === 1 && (
-        <div className="flex flex-col items-center justify-center space-y-4">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-            List ingredients
-          </h1>
-          {JSON.stringify(response)}
-        </div>
-      )}
+              <label htmlFor="icon-button-file">
+                <p className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 py-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50">
+                  Open Camera
+                </p>
+                <input
+                  className="hidden"
+                  name="image"
+                  accept="image/*"
+                  id="icon-button-file"
+                  type="file"
+                  capture="environment"
+                />
+              </label>
+              <button
+                type="submit"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 py-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
+        {/* list ingredients */}
+        {step === 1 && (
+          <div className="mt-20">
+            <Loading />
+          </div>
+        )}
+        {/* generate recipes */}
+        {step === 2 && response && (
+          <div>
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              Here are some recipes you can make
+            </h1>
+            <ul>{JSON.stringify(response)}</ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
